2e66ce38e558d9507689b35a94f37b3713a5ac20
vladbulaniy <vladbulanyy@gmail.com>
2021-04-28 12:12:54 +0300

    [02] Using custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from  '@angular/forms'
+//import {HttpModule} from  '@angular/http'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    //HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
